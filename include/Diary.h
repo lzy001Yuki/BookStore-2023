b4107e6d8bbde6a4a863af92c46e08df3120cdf8
lzy <147520993+lzy001Yuki@users.noreply.github.com>
2023-12-24 12:20:28 +0800

    fix RE problem because of string

--- include/Diary.h
+++ include/Diary.h
@@ -5,17 +5,27 @@
 #ifndef BOOKSTOREFINAL_DIARY_H
 #define BOOKSTOREFINAL_DIARY_H
 #include "Block.h"
+#include <iomanip>
+#include "User.h"
+
+class UserAll;
 class Record{
     friend class Diary;
 private:
     char userid[66] = {'\0'};
-    std::string operation;
+    char operation[1000] = {'\0'};
     int permission = 0;
+    double cost = -1.0;
+    bool status = true;
 public:
     Record() = default;
-    explicit Record(const char *userid_, std::string str, int per) :permission(per){
+    explicit Record(const char *userid_, const char *op, int per) :permission(per){
         strcpy(userid, userid_);
-        operation = std::move(str);
+        strcpy(operation, op);
+    }
+    explicit Record(const char* userid_, const char *op, int per, double cost_, bool sta) :permission(per), cost(cost_), status(sta){
+        strcpy(userid, userid_);
+        strcpy(operation, op);
     }
 
 };
@@ -24,15 +34,14 @@ public:
 class Diary{
 private:
     MemoryRiver<Record, 1> diary;
-    //int total = 0;
 public:
     Diary();
 
     void write_diary(Record &tmp);
 
-    void read_employee();
+    void read_employee(UserAll &user_all);
 
-    void read_all();
+    void read_all(UserAll &user_all);
 };
 
 
