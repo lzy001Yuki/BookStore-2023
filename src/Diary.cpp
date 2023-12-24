ff5a98dfdb347387cfb6c7d2c9c7495b671f3995
lzy <147520993+lzy001Yuki@users.noreply.github.com>
2023-12-24 12:18:16 +0800

    complete diary.cpp

--- src/Diary.cpp
+++ src/Diary.cpp
@@ -18,24 +18,68 @@ void Diary::write_diary(Record &tmp) {
     diary.write(tmp, 4 + (total - 1) * sizeof(Record), 1);
 }
 
-void Diary::read_employee() {
-
-}
-//
-void Diary::read_all() {
-    /*int total;
+void Diary::read_employee(UserAll &user_all) {
+    if (user_all.LogUsers.empty()) throw InvalidExp();
+    if (user_all.LogUsers.back().permission != 7) throw InvalidExp();
+    int total;
     diary.get_info(total, 1);
-    //auto *all = new Record[total];
-    //Record all[10000];
-    //diary.read(all[0], 4, total);
-    std::cout<<"-- Here is your log --"<<'\n';
-    for (int i = 0; i < total; i++) {
-        std::cout<<"User(ID):"<<'\t'<<all[i].userid<<'\t'<<all[i].operation<<'\n';
+    int num = 0;
+    num = total / 2000 + 1;
+    std::cout<<"--Here is your employee's report--"<<'\n';
+    for (int i = 0; i < num; i++) {
+        Record all[2000];
+        diary.read(all[0], 4 + i * 2000 * sizeof(Record), 2000);
+        int end;
+        if (i != num - 1) end = 2000;
+        else end = total % 2000;
+        for (int j = 0; j < end; j++) {
+            if (all[j].permission == 3) {
+                if ((all[j].cost == -1.0 && !all[j].status) || (all[j].cost != -1.0)) {
+                    // 是buy 或者 import
+                    if (!all[j].status) {
+                        std::cout << all[j].userid << '\t' << all[j].operation << "  ---------cost: " << '\t' << std::fixed
+                                  << std::setprecision(2) << all[j].cost << '\n';
+                    } else {
+                        std::cout << all[j].userid << '\t' << all[j].operation << "  ---------profit:" << '\t'
+                                  << std::fixed << std::setprecision(2) << all[j].cost << '\n';
+                    }
+                } else {
+                    std::cout << all[j].userid << '\t' << all[j].operation << '\n';
+                }
+            }
+        }
     }
-    for (int i = 0; i < total; i++) {
-        Record tmp;
-        diary.read(tmp, 4 + i * sizeof(Record) , 1);
-        std::cout<<"User(ID):"<<'\t'<<tmp.userid<<'\t'<<tmp.operation<<'\n';
+}
+
+//
+void Diary::read_all(UserAll &user_all) {
+    if (user_all.LogUsers.empty()) throw InvalidExp();
+    if (user_all.LogUsers.back().permission != 7) throw InvalidExp();
+    int total;
+    diary.get_info(total, 1);
+    int num = 0;
+    num = total / 2000 + 1;
+    std::cout<<"--Here is your log--"<<'\n';
+    std::cout<<"** Total Operation **"<<'\t'<<total<<'\n';
+    for (int i = 0; i < num; i++) {
+        Record all[2000];
+        diary.read(all[0], 4 + i * 2000 * sizeof(Record), 2000);
+        int end;
+        if (i != num - 1) end = 2000;
+        else end = total % 2000;
+        for (int j = 0; j < end; j++) {
+           if ((all[j].cost == -1.0 && !all[j].status) ||(all[j].cost != -1.0)) {
+                // 是buy 或者 import
+                if (!all[j].status) {
+                    std::cout<<all[j].userid<<'\t'<<all[j].operation;
+                    std::cout<<"  ---------cost: "<<'\t'<<std::fixed<<std::setprecision(2)<<all[j].cost<<'\n';
+                } else {
+                    std::cout<<all[j].userid<<'\t'<<all[j].operation;
+                    std::cout<<"  ---------profit:"<<'\t'<<std::fixed<<std::setprecision(2)<<all[j].cost<<'\n';
+                }
+            } else {
+                std::cout<<all[j].userid<<'\t'<<all[j].operation<<'\n';
+            }
+        }
     }
-    //delete [] all;*/
 }
